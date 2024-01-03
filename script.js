ddd57f66460b80fc642f2a176fec8417cefd4b0f
maicaocaa <maria.caocaa@gmail.com>
2024-01-03 17:26:40 +0100

    createProfileCard ok , repository link not ready

--- script.js
+++ script.js
@@ -1,25 +1,47 @@
-const APIURL = 'https://api.github.com/users/';
+
 const main = document.getElementById('main');
 const form  = document.getElementById('form')
 const searchInput = document.getElementById('search')
 
 form.addEventListener('submit', function (event){
- event.preventDefault();
- const searchUser = searchInput.value;
- console.log (searchUser);
-})
+   event.preventDefault();
+   const searchUser = searchInput.value;
+   console.log (searchUser);
+   getProfile(searchUser);
+  })
 
-// function getProfile() {
-//     axios.get(APIURL)
-//        .then((response)=>{
-//           const profile = response.data;
-//           console.log(response)
 
-//        });
-//        .catch((err) => {
-//         console.log(err);
-//        });
+function getProfile(searchUser) {
+    const APIURL = `https://api.github.com/users/${searchUser}`;
+    axios.get(APIURL)
+       .then((response)=>{
+          const profile = response.data;
+          createProfileCard (profile);
+          console.log (profile);
+       })
+    //    .catch((error) => {
+    //      console.log(error);
+    //     });
+};
 
-// };
-
-// getProfile();
+function createProfileCard (profile){
+    const createProfileCard =
+		` <div class="card">
+				<img class="avatar" src="${profile.avatar_url}" alt="profile avatar">
+ 			 <div class="user-info">
+              <h2>${profile.name}</h2>
+              <p>${profile.bio}</p>
+              <ul>
+                 <li>${profile.followers} <strong>Followers</strong> </li>
+                 <li> ${profile.following}<strong> Following </strong>git a</li>
+                 <li> ${profile.public_repos}<strong>Repos</strong> </li>
+              </ul>
+              <div class="repo">repositorios</div>
+              <div class="repo">repositorios2 </div>
+              <div class="repo">repositorios2 </div>
+              <div class="repo">repositorios2 </div>
+              <div class="repo">repositorios2 </div>
+ 			 </div>
+ 		</div>`
+         main.innerHTML = createProfileCard;
+  }
