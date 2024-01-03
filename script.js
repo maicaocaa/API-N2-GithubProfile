1ddbdceca3f7aa84eda1e801f996626dfeb4e51e
maicaocaa <maria.caocaa@gmail.com>
2024-01-03 13:15:51 +0100

    save form value

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-const APIURL = 'https://api.github.com/users/'
+const APIURL = 'https://api.github.com/users/';
+const main = document.getElementById('main');
+const form  = document.getElementById('form')
+const searchInput = document.getElementById('search')
+
+form.addEventListener('submit', function (event){
+ event.preventDefault();
+ const searchUser = searchInput.value;
+ console.log (searchUser);
+})
+
+// function getProfile() {
+//     axios.get(APIURL)
+//        .then((response)=>{
+//           const profile = response.data;
+//           console.log(response)
+
+//        });
+//        .catch((err) => {
+//         console.log(err);
+//        });
+
+// };
+
+// getProfile();
